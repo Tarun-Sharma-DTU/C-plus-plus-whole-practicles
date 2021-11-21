acd18813092b82a05d243ac0a3dbf219f1c78ae8
Tarun-Sharma-DTU <tarunsharma_it20b11_35@dtu.ac.in>
2021-11-22 01:43:22 +0530

    Addition Program Added

---
+++ Addition_of_two_numbers_taken_by_user.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<<"\t\t\t\tAddition of Two Numbers.\n\n\n";
+    cout<<"Enter Your First Number : ";
+    cin>>num1;
+    cout<<"\nEnter Your Second Number : ";
+    cin>>num2;
+    cout<<"\n\n\nSum of Both The Numbers is : "<<num1+num2<<endl;
+    
+    return 0;
+}
